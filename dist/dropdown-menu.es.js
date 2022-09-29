cd8dd868430658fd720e0927cfa76bcc6fa9c317
Jacob Lockard <jacoblockard99@gmail.com>
2022-09-29 13:25:31 -0400

    2.0.0-beta.1

--- dist/dropdown-menu.es.js
+++ dist/dropdown-menu.es.js
@@ -1,68 +1,74 @@
-import { h as m, resolveComponent as u, openBlock as d, createElementBlock as f, normalizeClass as w, createVNode as h, withCtx as _, renderSlot as b } from "vue";
-function l(r, s) {
-  r.props.class = `${r.props.class || ""} ${s}`.trim();
+import { h as d, resolveComponent as m, openBlock as u, createElementBlock as f, normalizeClass as w, createVNode as h, withCtx as y, renderSlot as g } from "vue";
+function s(e, t) {
+  e.props.class = `${e.props.class || ""} ${t}`.trim();
 }
-function p(r, s) {
-  return (e) => {
-    typeof s == "function" && s(e), e.cancelBubble || r(e);
+function i(e, t) {
+  return (n) => {
+    typeof t == "function" && t(n), n.cancelBubble || e(n);
   };
 }
-function a(r, s) {
-  return r.attrs.on[s] || r.type && r.type.listeners && r.componentOptions.listeners[s];
+function p(e, t) {
+  return e.attrs.on[t] || e.type && e.type.listeners && e.componentOptions.listeners[t];
 }
-const g = (r, s) => {
-  const e = s.slots.default()[0].children;
-  return e.forEach((t) => {
+function _(e) {
+  return e && e.type && (e.type === "fragment" || typeof e.type == "symbol");
+}
+function a(e) {
+  for (const t of e) {
+    if (_(t))
+      return a(t.children);
     t.props = Object.assign({ class: void 0 }, t.props), t.attrs = Object.assign({}, t.attrs), t.attrs.on || (t.attrs.on = {});
-    const n = t.props.class && t.props.class.match(/dropdown-item/), i = t.props.class && t.props.class.match(/dropdown-divider/);
-    t.attrs.on.click = p((o) => {
-      s.parent.$emit("click-item", o, t);
-    }, a(t, "click")), t.attrs.on.blur = p((o) => {
-      s.parent.$emit("blur-item", o, t);
-    }, a(t, "blur")), typeof t.type == "string" && t.type.match(/^h\d$/) ? l(t, "dropdown-header") : t.type === "hr" && !i ? (t.type = "div", l(t, "dropdown-divider")) : !n && !i && l(t, "dropdown-item");
-  }), m("div", {}, e);
-}, y = g, $ = (r, s) => {
-  const e = r.__vccOpts || r;
-  for (const [t, n] of s)
-    e[t] = n;
+    const n = t.props.class && t.props.class.match(/dropdown-item/), r = t.props.class && t.props.class.match(/dropdown-divider/);
+    t.attrs.on.click = i((o) => {
+      context.parent.$emit("click-item", o, t);
+    }, p(t, "click")), t.attrs.on.blur = i((o) => {
+      context.parent.$emit("blur-item", o, t);
+    }, p(t, "blur")), typeof t.type == "string" && t.type.match(/^h\d$/) ? s(t, "dropdown-header") : t.type === "hr" && !r ? (t.type = "div", s(t, "dropdown-divider")) : !n && !r && s(t, "dropdown-item");
+  }
   return e;
-}, k = {
+}
+const b = (e, t) => d("div", {}, a(t.slots.default())), $ = b, k = (e, t) => {
+  const n = e.__vccOpts || e;
+  for (const [r, o] of t)
+    n[r] = o;
+  return n;
+}, C = {
   name: "DropdownMenu",
   components: {
-    DropdownMenuItems: y
+    DropdownMenuItems: $
   },
   props: {
     align: {
       type: String,
       default: "left",
-      validate(r) {
-        return ["left", "right"].indexOf(r.toLowerCase()) !== -1;
+      validate(e) {
+        return ["left", "right"].indexOf(e.toLowerCase()) !== -1;
       }
     },
     show: Boolean
   }
-}, C = ["aria-labelledby"];
-function D(r, s, e, t, n, i) {
-  const o = u("dropdown-menu-items");
-  return d(), f("div", {
+}, D = ["aria-labelledby"];
+function I(e, t, n, r, o, M) {
+  const l = m("dropdown-menu-items");
+  return u(), f("div", {
     class: w(["dropdown-menu", {
-      "dropdown-menu-left": e.align === "left",
-      "dropdown-menu-right": e.align === "right",
-      show: e.show
+      "dropdown-menu-left": n.align === "left",
+      "dropdown-menu-right": n.align === "right",
+      show: n.show
     }]),
-    "aria-labelledby": r.$attrs.id
+    "aria-labelledby": e.$attrs.id
   }, [
-    h(o, null, {
-      default: _(() => [
-        b(r.$slots, "default", {
-          onClick: s[0] || (s[0] = (...c) => r.onItemClick && r.onItemClick(...c))
+    h(l, null, {
+      default: y(() => [
+        g(e.$slots, "default", {
+          onClick: t[0] || (t[0] = (...c) => e.onItemClick && e.onItemClick(...c))
         })
       ]),
       _: 3
     })
-  ], 10, C);
+  ], 10, D);
 }
-const O = /* @__PURE__ */ $(k, [["render", D]]);
+const v = /* @__PURE__ */ k(C, [["render", I]]);
 export {
-  O as DropdownMenu
+  v as DropdownMenu
 };
